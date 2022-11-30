aacabe4f9370c3ad2a486542a41bb0c3668603b0
mohammad <mk0782212@gmail.com>
2022-11-30 21:00:27 +0430

    first-version

---
+++ auto-move-screen.js
@@ -0,0 +1,35 @@
+let scrollerId;
+let paused = true;
+let speed = 3;
+let interval = speed*5;
+
+function startScroll(){
+    let id = setInterval(function(){
+         window.scrollBy(0,2);
+          if(
+            window.innerHeight + window.scrollY ==
+            document.body.offsetHeight
+        ) {
+
+        stopScroll();
+    }
+            },interval);
+          return id;
+    }
+
+    function stopScroll(){
+        clearInterval(scrollerId);
+    }
+            
+            document.body.addEventListener('keypress', function(event){
+            if (event.which==13 || event.keyCode ==13){
+
+        if (paused == true) {
+            scrollerId = startScroll();
+            paused = false;
+        }else{
+        stopScroll();
+        paused = true;
+        }
+}
+});
