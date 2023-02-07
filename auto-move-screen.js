9d8b25ec2f5ed795f8a961d109b0679ac8ba84f9
Mohammad karimi <116438550+ShahMkarimi@users.noreply.github.com>
2023-02-07 10:44:58 +0430

    Update auto-move-screen.js

--- auto-move-screen.js
+++ auto-move-screen.js
@@ -4,6 +4,7 @@ let paused = true;
 let speed = 3;
 let interval = speed*7;  //you can change the speed form here by the change of "5" number.
 
+
 function startScroll() {
     let id = setInterval(function() {
          window.scrollBy(0,2);
