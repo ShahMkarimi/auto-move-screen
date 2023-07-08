cd2e17d09e8ea3f4763d11a9d8422806f237def3
Mohammad karimi <116438550+ShahMkarimi@users.noreply.github.com>
2023-07-08 18:39:58 +0430

    Update auto-move-screen.js
    new edit

--- auto-move-screen.js
+++ auto-move-screen.js
@@ -3,47 +3,32 @@
 let scrollerId;
 let paused = true;
 let speed = 3;
-let interval = speed*7;  //you can change the speed form here by the change of "5" number.
-
+let interval = speed * 7; //you can change the speed form here by the change of "5" number.
 
 function startScroll() {
-    let id = setInterval(function() {
-         window.scrollBy(0,2);
-          if (
-              
-            window.innerHeight + window.scrollY ==
-            document.body.offsetHeight
-        ) {
-
-        stopScroll();
+  let id = setInterval(function () {
+    window.scrollBy(0, 2);
+    if (window.innerHeight + window.scrollY == document.body.offsetHeight) {
+      stopScroll();
     }
-        
-        
-            },interval);
-          return id;
-    }
+  }, interval);
+  return id;
+}
 
-    function stopScroll() {
-        clearInterval(scrollerId);
-    }
-            
-            document.body.addEventListener('keypress', function(event){
-            if (event.which==13 || event.keyCode ==13){//change the control key by the code key number
+function stopScroll() {
+  clearInterval(scrollerId);
+}
 
-        if (paused == true) {
-            scrollerId = startScroll();
-            paused = false;
-            
-        }
-                
-                
-       else {
-        stopScroll();
-        paused = true;
-        }
-}   
-      
-                
+document.body.addEventListener("keypress", function (event) {
+  if (event.which == 13 || event.keyCode == 13) {
+    //change the control key by the code key number
+
+    if (paused == true) {
+      scrollerId = startScroll();
+      paused = false;
+    } else {
+      stopScroll();
+      paused = true;
+    }
+  }
 });
-
-
