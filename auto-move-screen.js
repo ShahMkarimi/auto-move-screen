2d4781dc90aee103e8c62802e5272f4818a232a9
Mohammad karimi <116438550+ShahMkarimi@users.noreply.github.com>
2023-07-12 11:07:09 +0430

    Update auto-move-screen.js

--- auto-move-screen.js
+++ auto-move-screen.js
@@ -3,7 +3,7 @@
 let scrollerId;
 let paused = true;
 let speed = 3;
-let interval = speed * 7; //you can change the speed form here by the change of "7" number.
+let interval = speed * 7; //you can change the speed form here by the change of "7 seconde" number.
 
 function startScroll() {
   let id = setInterval(function () {
